05ede3bf65a85ee997a862b4b348400f5af07837
jjhiggz <jonathan.higger@gmail.com>
2020-05-07 16:28:30 -0600

    That was alot easier than I thought, the hooks are working now

--- src/App.js
+++ src/App.js
@@ -1,60 +1,43 @@
-import React from 'react'
 import AllWordContainer from './containers/AllWordContainer'
 import TopicMenu from './containers/TopicMenu'
 import ThemeBox from './containers/ThemeBox'
 import Timer from './customHooks/Timer'
+import React, { useState, useEffect } from 'react';
 
-export default class App extends React.Component{
-  state = {
-    words: [],
-    topic: [],
-    // timeLeft: 5,
-    gameActive: false,
-  }
-
-
-  // startTimer = () => {
-  //   let interval = setInterval( () => {
-  //     this.setState({ timeLeft: this.state.timeLeft - 1})
-  //   }, 1000)
-  //   if(this.state.timeLeft === 0){ clearInterval(interval) }
-  // }
-
-
-  sample = (array) => {
+export default function App(props){
+  const [words, setWords] = React.useState([])
+  const [topic, setTopic] = React.useState('')
+  const [gameActive, setGameActive] = React.useState('')
+  
+  const sample = (array) => {
     return array[Math.floor ( Math.random() * array.length )]
   }
-  getWords = (topic) => {
+  const getWords = (topic) => {
     fetch('http://localhost:9000/topics')
       .then(response => response.json())
-      .then(topics => this.findTopic(topic,topics))
-
+      .then(topics => findTopic(topic,topics))
   }
 
-  findTopic = ( topicName, topicsArray ) => {
+  const findTopic = ( topicName, topicsArray ) => {
     const topicObject = topicsArray.find( top => {
       return top.name === topicName
     })
     const {name, words} = topicObject
-    this.createGameWords(JSON.parse(words))
+    createGameWords(JSON.parse(words))
   }
 
-  createGameWords(words){
+  const createGameWords = (words) => {
     let newWords = []
     for(let i=0; i < 300; i++){
-      newWords.push(this.sample(words))
+      newWords.push(sample(words))
     }
+    newWords = newWords
+      .join(' ')
+      .split(' ')
+      .filter(term => term !== '')
 
-  newWords = newWords
-    .join(' ')
-    .split(' ')
-    .filter(term => term !== '')
-  this.setState({words: newWords})
-}
-
-  render(){
-    const  { getWords, updateGameActive } = this
-    const { words, gameActive } = this.state
+    setWords(newWords)
+  }
     return(
       <>
         <ThemeBox/>
@@ -67,5 +50,5 @@ export default class App extends React.Component{
         />
         <Timer/>
       </>
-    )}
+    )
 }
